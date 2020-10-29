a24a501605569cde0fb7b5fbbb1be9c301390613
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-29 12:21:14 +0600

    mobile design is done

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -4,16 +4,16 @@ const JobList = ({job}) => {
     const langAndTools = [...job.languages, ...job.tools];
 
     return (
-        <div className={`flex bg-white shadow-md m-4 p-6 rounded ${job.featured && 'border-l-4 border-teal-400 border-solid'}`}>
-            <div>
-                <img src={job.logo} alt={job.company} />
+        <div className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded ${job.featured && 'border-l-4 border-teal-400 border-solid'} md:flex-row`}>
+            <div className="mr-5 -mt-16 mb-4 md:mt-0 md:mb-0">
+                <img className="h-20 w-20 md:h-full md:w-full" src={job.logo} alt={job.company} />
             </div>
 
-            <div className="ml-5 flex justify-center flex-col">
+            <div className="flex justify-center flex-col">
                 <h3 className="font-bold text-teal-400">
                     <span className="mr-3">{job.company}</span>
-                    {job.isNew && <span className="bg-teal-400 text-teal-100 font-bold rounded-full py-1 px-2 ml-2">New</span>}
-                    {job.featured && <span className="bg-gray-900 text-white font-bold rounded-full py-1 px-2 ml-3">Featured</span>}
+                    {job.isNew && <span className="bg-teal-400 text-teal-100 font-bold rounded-full py-1 px-2 ml-2 uppercase">New</span>}
+                    {job.featured && <span className="bg-gray-900 text-white font-bold rounded-full py-1 px-2 ml-3 uppercase">Featured</span>}
                 </h3>
                 <h2 className="font-bold text-xl">{job.position}</h2>
                 <p className="text-gray-700">
@@ -21,8 +21,8 @@ const JobList = ({job}) => {
                 </p>
             </div>
 
-            <div className="flex items-center ml-auto">
-                {langAndTools ? langAndTools.map((langAndTool, index) => <span key={index} className="bg-teal-100 text-teal-600 bd-teal-100 font-bold m-2 px-3 py-2 rounded cursor-pointer">{langAndTool}</span>) : ''}
+            <div className="flex items-center mt-4 pt-4 border-t border-gray-500 border-solid md:ml-auto md:border-0 md:mt-0 md:pt-0">
+                {langAndTools ? langAndTools.map((langAndTool, index) => <span key={index} className="bg-teal-100 text-teal-600 font-bold mr-4 px-3 py-2 rounded cursor-pointer">{langAndTool}</span>) : ''}
             </div>
         </div>
     )
