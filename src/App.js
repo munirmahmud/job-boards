54a3b49c6662732fe22adfe1f5045f6eb1ed1546
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-29 11:28:35 +0600

    Header component added along with styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 import JobLists from './components/JobLists';
 
 const App = () => {
     return (
         <>
+            <Header />
             <JobLists />
         </>
     )
